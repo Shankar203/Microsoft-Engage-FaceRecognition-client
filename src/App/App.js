f3f7c0ebe17ce8b1d055ef04aa6b95355b9f4e29
shankar <gowrishankarirwog@gmail.com>
2022-05-25 15:59:19 +0530

    hashrouter change

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "../routes/Login";
 import Signup from "../routes/Signup";
 import "../styles/App.css";
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/signup" element={<Signup />} />
 				<Route path="/login" element={<Login />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
